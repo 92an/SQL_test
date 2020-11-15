2fbde7d74d19705a46768e79607b721bd29988da
92an <micke92an@comhem.se>
2020-11-15 17:01:45 +0000

    starting new project

---
+++ mongo.py
@@ -0,0 +1,52 @@
+import os
+import pymongo
+from pymongo import MongoClient
+
+
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myFirstDB"
+COLLECTION = "celeberty"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFaliure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+# inserting example
+""" new_docs = [{
+    "first": "Terry",
+    "last": "Pratchet",
+    "dob": "28/04/1948",
+    "gender": "M",
+    "hair_color": "Not much",
+    "occupation": "Writer",
+    "nationality": "Brittish "
+}, {
+    "first": "George",
+    "last": "RR Martin",
+    "dob": "20/09/1948",
+    "gender": "M",
+    "hair_color": "White",
+    "occupation": "Writer",
+    "nationality": "American"
+    }]
+
+coll.insert_many(new_docs) """
+
+documents = coll.find({"first": "douglas"})
+
+for doc in documents:
+    print(doc)
--- mysql-from-python-first.py
+++ mysql-from-python-first.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username
@@ -11,14 +12,26 @@ connection = pymysql.connect(
                             user=username,
                             password="",
                             db="Chinook")
-try:
-    # Run a Query
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
 
+# Insert example:
+""" try:
+    with connection.cursor() as cursor:
+        row = [("Bob", 21, "1990-02-10 23:10:10"), 
+                ("Fred", 61, "1960-02-10 20:10:10"),
+                ("Jim", 101, "1920-02-10 15:10:10")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        connection.commit()
+finally:
+    # Close connection.
+    connection.close() """
+
+
+try:
+    with connection.cursor() as cursor:
+        list_of_names = ['Fred', 'Bob']
+        format_strings = ",".join(["%s"]*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names )
+        connection.commit()
 finally:
     # Close connection.
     connection.close()
